98326da48ba4faac38d264aed9ef546525026fa1
Alexander <protonull@protonmail.com>
2022-12-31 07:42:05 +0000

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    id("java")
+    // ShadowJar (https://github.com/johnrengelman/shadow/releases)
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+group = "uk.protonull.pistomqueue"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    // For Minestom
+    maven("https://jitpack.io")
+}
+
+dependencies {
+
+    implementation("com.github.Minestom:Minestom:-SNAPSHOT") {
+        isChanging = true
+        constraints {
+            implementation("commons-net:commons-net:3.9.0")
+        }
+    }
+
+    compileOnly(libs.lombok)
+    annotationProcessor(libs.lombok)
+    testCompileOnly(libs.lombok)
+    testAnnotationProcessor(libs.lombok)
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+tasks {
+    jar {
+        manifest {
+            attributes["Main-Class"] = "uk.protonull.pistomqueue.Main"
+        }
+    }
+    build {
+        dependsOn(shadowJar)
+    }
+    test {
+        useJUnitPlatform()
+    }
+    shadowJar {
+        mergeServiceFiles()
+        archiveClassifier.set("") // Prevent the -all suffix
+    }
+}
---
+++ src/main/java/uk/protonull/pistomqueue/Main.java
@@ -0,0 +1,12 @@
+package uk.protonull.pistomqueue;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Main {
+
+    public void main(final String[] args) {
+
+    }
+
+}
